66c556d1f295bd97d5656907fb03e0b45b8abe57
viniciusgpg <vinygp8@gmail.com>
2021-01-07 11:39:14 -0300

    Atualizando settings.py

--- prevencao_ml/prevencao_ml/settings.py
+++ prevencao_ml/prevencao_ml/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'prevencao_ml'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'prevencao_ml.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
