fb0e79e71a99affe79d0cfde0e850b7fc45e66c0
viniciusgpg <vinygp8@gmail.com>
2021-01-07 13:55:17 -0300

    Update views.py

--- prevencao_ml/prevencao_ml/views.py
+++ prevencao_ml/prevencao_ml/views.py
@@ -11,8 +11,6 @@ def getPredictions(peso, altura, imc, sistolica, diastolica, usa_medicamentos,
        parou_fumar, bebe, qtd_bebida, usa_droga, atv_fisica,
        hora_atv_fisica, alimentacao_saudavel, ansiedade, estresse):
     import pickle
-    from django.contrib.staticfiles import finders
-    print(prevencaopath)
     model = pickle.load(open('prevencao.sav', "rb"))
     prediction = model.predict([[peso, altura, imc, sistolica, diastolica, usa_medicamentos,
        cirurgia, diabetes, depressao, dor_cronica, hipertensao,
